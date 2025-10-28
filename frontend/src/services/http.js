d1ff788d414b7dd2e8db9547974cbddcb1aaabf8
VIP <maiduongtienphuocloc@gmail.com>
2025-10-29 00:39:24 +0700

    up BE

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useUserStore } from "@/stores/user";
 
-const APP_API_URL = "http://localhost:8088/api/";
+const APP_API_URL = import.meta.env.VITE_API_URL;
 // const Cookie = require("js-cookie");
 export default class Http {
   constructor(status) {
